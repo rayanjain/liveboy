53a0b8bb70d0cca339886ab982c3e55db344aace
Rayan Jain Ajmera <rayanjain@Rayans-MacBook-Pro.local>
2021-04-18 00:26:57 +0530

    api addition

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://192.168.29.145:3000'
+axios.defaults.baseURL = 'https://liveboy.herokuapp.com'
 
 createApp(App)
   .use(store)
